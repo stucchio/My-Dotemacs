daa2eb764ddc32c83ea8cfcc31be065869f93c07
stucchio <stucchio@math.rutgers.edu>
2007-05-16 23:39:13 -0400

    Fixed bug in darcsum-no-duplicate-buffer.
    If there are no changes, this function would still try to switch to buffer named darcs-buffer-name.
    Since it did not exist, this would change the name of the current buffer to darcs-buffer-name, a bad thing.
    
    Now it simply prints a message "No changes.", which is what it should have did all the time.
    
    darcs-hash:20070517033913-00070-7af843603e393183f1c61f18b6dc1cb7f6754910.gz

--- my_lisp/darcsum-custom.el
+++ my_lisp/darcsum-custom.el
@@ -1,5 +1,5 @@
 (defun darcsum-no-duplicate-buffer ()
-  "Opens new darcsum window, or jumps to current if it already exists. The new darcsum window is opened in the current directory (or parent directory if that's where darcs is).."
+  "Opens new darcsum window, or jumps to current if it already exists. The new darcsum window is opened in the current directory (or parent directory if that's where darcs is)."
   (interactive)
   (let ( (working-dir default-directory) )
     (let ( (darcs-buffer-name (concat "*darcs-" working-dir "*")) )
@@ -9,11 +9,13 @@
 	      (switch-to-buffer darcs-buffer)
 	      (darcsum-refresh)
 	      )
-	  (progn
-	  (darcsum-whatsnew working-dir)
-	  (switch-to-buffer (get-buffer "*darcs*"))
-	  (rename-buffer darcs-buffer-name)
-	  (switch-to-buffer (get-buffer darcs-buffer-name))
-	  ))))))
+	  (if (darcsum-whatsnew working-dir)
+	      (progn
+		(switch-to-buffer (get-buffer "*darcs*"))
+		(rename-buffer darcs-buffer-name)
+		(switch-to-buffer (get-buffer darcs-buffer-name))
+		)
+	    (message "No changes")
+	    ))))))
 
 
