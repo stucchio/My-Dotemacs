c1634c71b2a5f6bb4bad0983c75d322db023812d
stucchio <stucchio@math.rutgers.edu>
2007-11-26 10:34:13 -0500

    When closing emacs, and processes are open, just kill them.
    darcs-hash:20071126153413-00070-8a2fb5a359c02b563ac6cf00eb5ee196d0b10470.gz

--- dotemacs.el
+++ dotemacs.el
@@ -246,5 +246,10 @@
 ;;******** Load browse-kill-ring ********
 (require 'browse-kill-ring)
 
+;;******** Save buffers without asking about killing processes ********
+(defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
+  "Prevent annoying \"Active processes exist\" query when you quit Emacs."
+  (flet ((process-list ())) ad-do-it))
+
 ;;******** Finally, set key bindings ********
 (load-file (concat emacs-root "keys.el"))
