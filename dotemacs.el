d39ab83cb1f1c2ad7e64b2202b3dda53f6112825
stucchio <stucchio@math.rutgers.edu>
2009-05-09 14:47:02 -0400

    Added clojure-mode to emacs.
    darcs-hash:20090509184702-00070-871b3aec33e7401d112678e8215e52212cabb8b9.gz

--- dotemacs.el
+++ dotemacs.el
@@ -20,7 +20,7 @@
 (add-path "site-lisp/gnuplot-mode") ;;Gnuplot support
 (add-path "site-lisp/python-mode") ;;Python mode
 (add-path "site-lisp/icicles") ;;Icicles, autocompletion
-(add-path "site-lisp/slime-2.0") ;;Slime lisp mode
+(add-path "site-lisp/slime-cvs") ;;Slime lisp mode
 
 (transient-mark-mode 1)
 (add-hook 'write-file-functions 'delete-trailing-whitespace)
@@ -202,13 +202,6 @@
 (autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
 (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
 
-;;********* Slime mode *********
-(setenv "SBCL_HOME" (concat (getenv "HOME") "/lib/sbcl")) ;;Need to set location of slime core path
-(setq inferior-lisp-program (concat (getenv "HOME") "/bin/sbcl"))
-;;(add-to-list 'load-path "the path of your slime directory")
-(require 'slime)
-(slime-setup)
-
 ;;********* Quack *********
 (require 'quack)
 (load-file (concat emacs-root "my_lisp/quack-custom.el"))
@@ -233,6 +226,43 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+;;********* Clojure mode *********
+;; Clojure mode
+(add-path "lisp/clojure/clojure-mode")
+(add-path "lisp/clojure/swank-clojure")
+(require 'clojure-mode)
+;; (require 'clojure-paredit) ; Uncomment if you use Paredit
+
+;; Slime
+;;(add-to-list 'load-path "/path/to/slime/")
+(require 'slime)
+(slime-setup)
+
+;; clojure swank
+(setq swank-clojure-jar-path (concat (getenv "HOME") "/src/clojure/clojure.jar"))
+;alternatively, you can set up the clojure wrapper script and use that:
+;(setq swank-clojure-binary "/path/to/cljwrapper")
+
+; you can also set up extra classpaths, such as the classes/ directory used by AOT compilation
+;(setq swank-clojure-extra-classpaths (list "/path/to/extra/classpaths" "/even/more/classpaths"))
+
+(add-path "lisp/clojure/swank-clojure")
+(require 'swank-clojure-autoload)
+
+;; The following function runs Slime with Clojure, even if Slime defaults to another Lisp.
+;; The above configuration alone, however, will make Clojure the default, so all that is necessary
+;; to run Slime with Clojure is M-x slime.
+(defun run-clojure ()
+  "Starts clojure in Slime"
+  (interactive)
+  (slime 'clojure))
+
+;; To use other Lisps...
+;; Incidentally, you can then choose different Lisps with
+;;   M-- M-x slime <tab>
+;; (add-to-list 'slime-lisp-implementations
+;;             '(sbcl   ("/path/to/bin/sbcl")))
 
 ;;********* Javascript mode *********
 (autoload 'js2-mode "js2" nil t)
