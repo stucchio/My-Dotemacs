ba0c87c40f1f20e72f004d6e40e9cbc7ff2c3933
stucchio <stucchio@math.rutgers.edu>
2009-08-21 10:31:06 -0400

    Added auto-hungry and auto-newline to c++ mode.
    darcs-hash:20090821143106-00070-64a7f13726dd9fdbaf13b0bc58b09299047d6855.gz

--- my_lisp/c-custom.el
+++ my_lisp/c-custom.el
@@ -1,2 +1,7 @@
 (add-hook 'c-mode-hook
           (lambda () (c-toggle-auto-hungry-state 1)))
+(add-hook 'c++-mode-hook
+          (lambda () (c-toggle-auto-hungry-state 1)))
+(add-hook 'c++-mode-hook
+          (lambda () (c-toggle-auto-newline 1)))
+
