114914be36e73ddf3f125562f86afd406854ff9a
stucchio <stucchio@math.rutgers.edu>
2009-12-04 00:01:48 -0500

    Added function to nxml-edit-javascript which opens up inline scripts in a temporary javascript-mode buffer.
    darcs-hash:20091204050148-00070-b52c5abef5db9ca2374146db705a30bfa6861193.gz

--- my_lisp/nxml-custom.el
+++ my_lisp/nxml-custom.el
@@ -19,8 +19,54 @@
     (tempo-template-nxml-paragraph)
     )
   )
-  
-(eval-after-load 'nxhtml
-  '(define-key nxhtml-mode-map [(M p)] 'xhtml-make-paragraph))
 
 (tempo-define-template "nxml-paragraph" '("<p>" p "</p>") )
+
+(defun nxml-edit-javascript ()
+  "This function opens a new javascript-mode buffer based on the current <script></script element, in the js2-mode."
+  (interactive)
+  (let ((s "")
+	(js-buffer (generate-new-buffer "inline-javascript.js"))
+	(original-buffer (current-buffer))
+	)
+
+    (save-excursion ;Check if we are in a javascript block
+      (condition-case nil
+	  (nxml-up-element)
+	  (error (error "Not in javascript block")))
+      (nxml-backward-element)
+      (unless (equal (nxml-parse-start-tag) '("script" (("type" . "text/javascript"))))
+	(error "Not in javascript block")
+	)
+      )
+
+      (save-excursion
+	(nxml-mark-paragraph)
+	(append-to-buffer js-buffer (point) (mark))
+	) ;Create temporary javascript buffer, append region to it
+
+      (switch-to-buffer js-buffer) ;Switch to that buffer
+      (javascript-mode) ;turn on javascript mode
+
+      ;remember where we came from
+      (mapcar 'make-variable-buffer-local '(kill-buffer-hook from-buffer jstemp-buffer))
+      (setq jstemp-buffer js-buffer)
+      (setq from-buffer original-buffer)
+
+      (add-hook 'kill-buffer-hook (lambda ()
+				    (let ((s (buffer-string)))
+				      (switch-to-buffer from-buffer)
+				      (nxml-mark-paragraph)
+				      (delete-region (point) (mark))
+				      (insert s)
+				      (switch-to-buffer from-buffer)
+				      )
+				    ))
+      )
+  )
+
+(eval-after-load 'nxhtml
+  '(progn
+     (define-key nxhtml-mode-map [(M p)] 'xhtml-make-paragraph)
+    (define-key nxhtml-mode-map "\C-j" 'nxml-edit-javascript))
+  )
